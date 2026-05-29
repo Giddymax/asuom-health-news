3303d642ed59fa53a8e844e5b3984e3a40015d12
Giddymax <gkpodonu@gmail.com>
2026-05-29 11:19:58 +0000

    AHN

--- components/site/site-footer.tsx
+++ components/site/site-footer.tsx
@@ -52,7 +52,20 @@ export async function SiteFooter() {
               </a>
             </li>
             <li>
-              <Link href="/admin/login">Dashboard Login</Link>
+              <Link href="/admin/login" className="footer-settings-link" aria-label="Dashboard login">
+                <svg
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M12 15.5A3.5 3.5 0 1 0 12 8a3.5 3.5 0 0 0 0 7.5Z" />
+                  <path d="M19.4 15a1.7 1.7 0 0 0 .34 1.88l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06A1.7 1.7 0 0 0 15 19.4a1.7 1.7 0 0 0-1 .6l-.04.08a2 2 0 0 1-3.84 0L10.08 20a1.7 1.7 0 0 0-1-.6 1.7 1.7 0 0 0-1.88.34l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.7 1.7 0 0 0 4.6 15a1.7 1.7 0 0 0-.6-1l-.08-.04a2 2 0 0 1 0-3.84L4 10.08a1.7 1.7 0 0 0 .6-1A1.7 1.7 0 0 0 4.26 7.2l-.06-.06A2 2 0 1 1 7.03 4.3l.06.06A1.7 1.7 0 0 0 9 4.6a1.7 1.7 0 0 0 1-.6l.04-.08a2 2 0 0 1 3.84 0L13.92 4a1.7 1.7 0 0 0 1 .6 1.7 1.7 0 0 0 1.88-.34l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.7 1.7 0 0 0 19.4 9c.22.37.47.72.6 1l.08.04a2 2 0 0 1 0 3.84L20 13.92a1.7 1.7 0 0 0-.6 1.08Z" />
+                </svg>
+              </Link>
             </li>
           </ul>
         </div>
